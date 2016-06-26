efcf0258b86f7daa7f1518d795aa078ee3510340
Skipper1931 <Skipper1931@users.noreply.github.com>
2016-06-26 07:44:58 -0400

    Added comments to piglatin.py

--- piglatin.py
+++ piglatin.py
@@ -1,15 +1,20 @@
+#Run with the Python Launcher for it to work
+
 pyg = 'ay'
 
 original = raw_input("Enter a word ")
 
+#Makes sure word is a word
 if len(original) > 0 and original.isalpha():
-    print("Your original word is: " + original)
-    word = original.lower()
-    first = word[0]
-    new_word = word + first + pyg
-    new_word = new_word[1:]
+  #Translates word
+  print("Your original word is: " + original)
+  word = original.lower()
+  first = word[0]
+  new_word = word + first + pyg
+  new_word = new_word[1:]
 else:
-    print("null")
+  print("null")
+#Prints translated word
 print("Your translated word is: " + new_word)
     
     
