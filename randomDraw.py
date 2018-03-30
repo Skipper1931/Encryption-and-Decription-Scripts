27cf4d5eefae77cf736bc2c3cb32ed71024c3248
Skipper1931 <aaronperrotta@MacBook-Aaron.local>
2018-03-30 07:47:27 -0400

    got randomDraw to work

--- randomDraw.py
+++ randomDraw.py
@@ -1,4 +1,4 @@
-# Random draw-er
+# Random line drawer
 # By Skipper1931
 
 # imports libs.
@@ -6,42 +6,46 @@ from random import *
 from turtle import *
 
 # gets user input
-randMin = input("Put in the minimum integer for the random number generator ")
-randMax = input("Put in the minimum integer for the random number generator ")
+randMin = input("Enter the minimum integer for the random number generator ")
+randMax = input("Enter the minimum integer for the random number generator ")
+randMin = int(randMin)
+randMax = int(randMax)
 
 # staring vars
 steps = randint(randMin,randMax)
-step = 0
-print steps
+step = 1
+stepPrint = str(steps)
+print("There will be " + stepPrint + " steps.")
 
 # while loop function because im too lazy to just use a loop (I like functions :P)
 def loop():
-	
-	# global variables because i always somehow endup using these dang things (I like 'em tho)
-	global steps
-	global step
-	global randMin
-	global randMax
 
-	# meat
-	if step != steps:
-		operation = randint(0,3)
-		opNum = randint(randMin,randMax)
-		print opNum
-		if operation == 1 and step != steps:
-			left(opNum)
-			forward(opNum)
-			end()
-		if operation == 2 and step != steps:
-			right(opNum)
-			forward(opNum)
-			end()
-		
-# actual useful func. here bois
-def end():
-	
-	# global var
-	global step
+    # global variables because i always somehow endup using these dang things (I like 'em tho)
+    global steps
+    global step
+    global randMin
+    global randMax
+    print("gottoloop")
 
-	step = step + 1
-	loop()
+    # meat
+    if step != steps + 1:
+	    operation = randint(1,2)
+	    opNum = randint(randMin,randMax)
+	    opNum = int(opNum)
+	    stp = str(step)
+	    if operation == 1:
+		    print("Step " + stp + ": Left")
+		    left(opNum)
+		    forward(opNum)
+		    step = step + 1
+		    operation = 0
+		    loop()
+	    elif operation == 2:
+		    print("Step " + stp + ": Right")
+		    right(opNum)
+		    forward(opNum)
+		    step = step + 1
+		    operation = 0
+		    loop()
+
+loop()
