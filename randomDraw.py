53a02437db1bd2065afae371ded9ad314f19a7a7
Skipper1931 <aaronperrotta@MacBook-Aaron.local>
2018-03-31 13:31:21 -0400

    Added colors for randomDraw and no-boundary support

--- randomDraw.py
+++ randomDraw.py
@@ -8,23 +8,34 @@ from datetime import *
 from tkinter import *
 
 # gets user input
-randMin = input("Enter the minimum integer for the random number generator ")
-randMax = input("Enter the maximum integer for the random number generator ")
+usercolor = input("Do you want to use color? (y or n)")
+
+randMin = input("Enter the minimum integer for the random number generator")
+randMax = input("Enter the maximum integer for the random number generator")
+
+xbound = input("What do you want the boundary to be for the x axis? (Enter 'd' to use the default, or 'n' to use no boundary)")
+ybound = input("What do you want the boundary to be for the y axis? (Enter 'd' to use the default, or 'n' to use no boundary)")
+
+if xbound == "N" or "D":
+    xbound = xbound.lower()
+if ybound  == "N" or "D":
+    ybound = ybound.lower()
+
+usercolor = usercolor.lower()
 randMin = int(randMin)
 randMax = int(randMax)
 
-xbound = input("What do you want the boundary to be for the x axis? (Enter 'd' to use the default)")
-ybound = input("What do you want the boundary to be for the y axis? (Enter 'd' to use the default)")
-
 if xbound == "d":
     xbound = 300
-else:
+elif xbound != "n":
     xbound = int(xbound)
 if ybound == "d":
     ybound = 250
-else:
+elif ybound != "n":
     ybound = int(ybound)
 
+
+colormode(255)
 
 # staring vars
 steps = randint(randMin,randMax)
@@ -41,23 +52,25 @@ while step != steps + 1:
     y = ycor()
     x = int(x)
     y = int(y)
-    xoff = xbound - 1
-    yoff = ybound - 1
     
-    if x >= xbound:
-        setx(xoff)
-        bounces = bounces + 1
-    elif x <= 0 - xbound:
-        setx(0 - xoff)
-        bounces = bounces + 1
-
-    if y >= ybound:
-        sety(yoff)
-        bounces = bounces + 1
-    elif y <= 0 - ybound:
-        sety(0 - yoff)
-        bounces = bounces + 1
-    
+    if xbound != "n":
+        xoff = xbound - 1
+        if x >= xbound and xbound != "n":
+            setx(xoff)
+            bounces = bounces + 1
+        elif x <= 0 - xbound and xbound != "n":
+            setx(0 - xoff)
+            bounces = bounces + 1
+            
+    if ybound != "n":
+        yoff = ybound - 1
+        if y >= ybound and ybound != "n":
+            sety(yoff)
+            bounces = bounces + 1
+        elif y <= 0 - ybound and ybound != "n":
+            sety(0 - yoff)
+            bounces = bounces + 1
+	
     # meat
     if step != steps + 1:
         # RNG
@@ -65,9 +78,19 @@ while step != steps + 1:
         opNum = randint(randMin,randMax)
  
         # sets up screen
-        screensize(2000,1500)
+        if xbound == "n" and ybound != "n":
+            screensize(300,ybound)
+        elif xbound != "n" and ybound == "n":
+            screensize(xbound,250)
+        elif xbound == "n" and ybound == "n":
+            screensize(300,250)
+        else:
+            screensize(xbound,ybound)
         
         # writes turtle to screen
+        if usercolor == "y":
+            color((randint(0,255),randint(0,255),randint(0,255)))
+
         if operation == 1:
             print("Step {}/{}: Left. {} Bounces".format(step,steps,bounces))
             detailPre = "Step {}/{}: Left. {} Bounces".format(step,steps,bounces)
@@ -91,6 +114,7 @@ while step != steps + 1:
             operation = 0
             opNum = 0
         detailLog = "{}\n{}".format(detailLog,detailPre)
+
 # Logs Stuff
 output = getscreen()
 user = input("Would you like to save a more detailed log? (y or n)")
@@ -98,8 +122,8 @@ user = user.lower()
 
 if step == steps + 1 and user != "y":
     date = datetime.now()
-    log = open("bounces.log","a+")
-    log.write("\n[{}] Bounces: {} Steps: {}".format(date.strftime("%Y-%m-%d %H:%M:%S"),bounces,steps))
+    log = open("randomDraw.log","a+")
+    log.write("\n[{}] Bounces: {} | Steps: {}".format(date.strftime("%Y-%m-%d %H:%M:%S"),bounces,steps))
     log.close()
     output.getcanvas().postscript(file="randomDrawOutput.eps")
     import thank
@@ -108,8 +132,8 @@ elif step == steps + 1 and user == "y":
     log = open("{}.log".format(date.strftime("%Y-%m-%d %H:%M:%S")),"a+")
     log.write("# Log for {}\n{}".format(date.strftime("%Y-%m-%d %H:%M:%S"),detailLog))
     log.close()
-    log = open("bounces.log","a+")
-    log.write("\n[{}] Bounces: {} Steps: {}".format(date.strftime("%Y-%m-%d %H:%M:%S"),bounces,steps))
+    log = open("randomDraw.log","a+")
+    log.write("\n[{}] Bounces: {} | Steps: {}".format(date.strftime("%Y-%m-%d %H:%M:%S"),bounces,steps))
     log.close()
     output.getcanvas().postscript(file="randomDrawOutput.eps")
     import thank
